7b127adce2c5f4005fc39dc8476fe8884fc8d2bc
Yanaki Krastev <ykrastev17@gmail.com>
2021-12-05 15:27:56 +0200

    Started the development of a more difficult project.

---
+++ TidalApplication/ControlClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidalApplication
+{
+    internal class ControlClass
+    {
+        public static int GetNumberForChoice()
+        {
+            string choiceAsString = Console.ReadLine();
+            int choiceAsInteger = 0;
+
+            while(!(int.TryParse(choiceAsString, out choiceAsInteger) && choiceAsInteger >=1 && choiceAsInteger <=5))
+            {
+                Console.WriteLine("Dear user, you have not inputted a number" +
+                    "\nbetween 1 and 5 inclusive. Please, try again");
+
+                choiceAsString = Console.ReadLine();
+            }
+
+            return choiceAsInteger;
+        }
+
+    }
+}
---
+++ TidalApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TidalApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MenuMethod();
+
+        }
+
+        public static void MenuMethod()
+        {
+            Console.WriteLine("Dear user, please pick an option from our menu:" +
+                "\n1 - Show the current playlist" +
+                "\n2 – Add a new CD including songs" +
+                "\n3 – Play" +
+                "\n4 – Shuffle" +
+                "\n5 – Stop the program");
+
+            int userChoice = ControlClass.GetNumberForChoice();
+
+            SwitchChoice(userChoice);
+        }
+
+        public static void SwitchChoice(int userChoice)
+        {
+            switch(userChoice)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("Dear user, we are still working on option " + userChoice +
+                        ".\nPlease, excuse us for any inconvenience. You will be" +
+                        "\nrefered back to the main menu.");
+                    MenuMethod();
+                    break;
+                case 5:
+                    Console.WriteLine("Dear user, you have decided to cancel" +
+                        "\nthe execution of the following program." +
+                        "\nThank you for using our services.");
+                    Environment.Exit(0);
+                    break;
+                
+            }
+        }
+    }
+}
